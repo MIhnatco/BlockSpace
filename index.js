85e8f69b4b611467d4df35ec6e6710c965d70886
Ihliana <Ihnatco@protonmail.com>
2023-11-03 08:14:42 +0100

    Update index.js
    clear the form after update and refactoring the code

--- index.js
+++ index.js
@@ -1,32 +1,69 @@
+ /**
+ * An array to store the list of posts.
+ * @type {Array}
+ */
+ let postsArr = [];
+
+ /**
+ * The input element for the post title.
+ * @type {HTMLInputElement}
+ */
+ const titleInput = document.getElementById('post-title')
+
+ /**
+ * The input element for the new  posts.
+ * @type {HTMLFormElement}
+ */
+ const bodyInput = document.getElementById('post-body')
+
+ /**
+ * The input element for the post title.
+ * @type {HTMLInputElement}
+ */
+ const form = document.getElementById("new-post")
+ 
+/**
+ * Renders the list of posts to the HTML page.
+ */
+ function renderPosts(){
+  let html = ""
+  for(let post of postsArr){
+      html += `
+          <h3>${post.title}</h3>
+          <p>${post.body}</p>
+          <hr>
+          `
+  }
+  document.querySelector('#blog-list').innerHTML = html;
+ }
+ 
+
+
+ // Fetch initial posts from the API
  fetch('https://apis.scrimba.com/jsonplaceholder/posts')
     .then(response => response.json())
     .then(data => {
-        const postArr = data.slice(0,5)
+        postsArr = data.slice(0,5)
         
-        let html = ""
-        for(let post of postArr){
-            html += `
-                <h3>${post.title}</h3>
-                <p>${post.body}</p>
-                <hr>
-                `
-        }
+        renderPosts()
 
-        document.querySelector('#blog-list').innerHTML = html;
     })
 
-
-document.getElementById('new-post').addEventListener("submit", (e) => {
+// Event listener for the form submission
+form.addEventListener("submit", (e) => {
       e.preventDefault();
 
-      const postTitle = document.getElementById('post-title').value; 
-      const postBody = document.getElementById('post-body').value;
+       // Get the values from the input fields
+      const postTitle = titleInput.value; 
+      const postBody = bodyInput.value;
 
+        // Create a new post object
       const post = {
         title: postTitle, 
         body: postBody
       }
 
+       // Define options for the POST request
       const options = {
         method: "POST", 
         body: JSON.stringify(post), 
@@ -35,20 +72,28 @@ document.getElementById('new-post').addEventListener("submit", (e) => {
         }
       }
 
+      / Send a POST request to add a new post
       fetch('https://apis.scrimba.com/jsonplaceholder/posts', options )
         .then(response => response.json())
         .then(data => {
-          console.log(data)
-            /**
-             * Challenge: Update the DOM with the new blog entry
+      
+          
+           /**
+             * Challenge: Use our new renderPosts function to clean up this code.
+             * 
+             * Don't forget to update the postsArray variable first!
              */
 
-            document.querySelector('#blog-list').innerHTML = `
-                <h3>${data.title}</h3>
-                <p>${data.body}</p>
-                <hr>
-                ${document.querySelector('#blog-list').innerHTML}
-            `
+           // Add the new post to the postsArr
+           postsArr.unshift(data)
+           renderPosts()
+
+            /**
+             * Challenge: clear the form out!
+             */
+            // Send a POST request to add a new post
+           titleInput.value = "";
+            bodyInput.value = "";
         })
 })
 
