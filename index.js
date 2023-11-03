7e2c1514ea6ee5042e9e20b442798ab5dcff59a8
Ihliana <Ihnatco@protonmail.com>
2023-11-03 07:46:50 +0100

    Update index.js
    sending data off to the server

--- index.js
+++ index.js
@@ -16,16 +16,6 @@
     })
 
 
-    /**
- Challenge:
- 
- * Listen for the "submit" event on the form (which will happen when the button is clicked)
-    * (Don't forget to preventDefault on the form so it doesn't refresh your page. Google "form preventDefault" if you're not sure what I'm talking about)
- * Combine the title value and body value into an object (with a "title" property and "body" property)
- * Log the object to the console
-
-*/
-
 document.getElementById('new-post').addEventListener("submit", (e) => {
       e.preventDefault();
 
@@ -37,5 +27,27 @@ document.getElementById('new-post').addEventListener("submit", (e) => {
         body: postBody
       }
 
-      console.log(post) //test the functionality
+      /**
+       * Challenge: Send this off to the server!
+       * 
+       * 1. BaseURL: https://apis.scrimba.com/jsonplaceholder/
+       * 2. Endpoint: /posts
+       * 3. method: ???
+       * 4. Request body: ??? (Remember to turn it into JSON)
+       * 5. Headers: ??? (Check the JSON Placeholder API docs or past casts for help)
+       */
+
+      const options = {
+        method: "POST", 
+        body: JSON.stringify(post), 
+        headers: {
+          "content-type": "application/json"
+        }
+      }
+
+      fetch('https://apis.scrimba.com/jsonplaceholder/posts', options )
+        .then(response => response.json())
+        .then(data => console.log(data))
 })
+
+
